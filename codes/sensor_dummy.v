2acab88079a5b9e663ec905968ff5ec924200562
K Subhamraj Patra <subhamrajpatra83@gmail.com>
2025-07-25 20:29:10 +0530

    Update sensor_dummy.v

--- codes/sensor_dummy.v
+++ codes/sensor_dummy.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10.07.2025 09:49:46
-// Design Name: 
-// Module Name: sensor_dummy
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module sensor_dummy(
     input wire clk,
     input wire rst,

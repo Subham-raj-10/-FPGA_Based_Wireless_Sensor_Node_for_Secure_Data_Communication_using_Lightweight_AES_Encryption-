e3ab06ea1a6b5fe076f653563cdaf6716d0f64ea
K Subhamraj Patra <subhamrajpatra83@gmail.com>
2025-07-25 20:28:30 +0530

    Update aes_128.v

--- codes/aes_128.v
+++ codes/aes_128.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10.07.2025 09:50:39
-// Design Name: 
-// Module Name: aes_128
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module aes_128(
     input wire clk,
     input wire rst,

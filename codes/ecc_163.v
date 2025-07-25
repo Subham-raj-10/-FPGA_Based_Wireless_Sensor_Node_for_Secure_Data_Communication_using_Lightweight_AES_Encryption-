7dde8b801189fe0372f8daf412318eb3cc6c5b4e
K Subhamraj Patra <subhamrajpatra83@gmail.com>
2025-07-25 20:28:51 +0530

    Update ecc_163.v

--- codes/ecc_163.v
+++ codes/ecc_163.v
@@ -1,25 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10.07.2025 09:51:32
-// Design Name: 
-// Module Name: ecc_163
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module ecc_163(
     input wire clk,
     input wire rst,
